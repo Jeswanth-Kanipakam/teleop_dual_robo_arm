f0088b47586f2b6f070994b906148dce66c4632b
Jeswanth_Kanipakam <jeswanthkanipakam@gmail.com>
2026-04-30 01:03:19 +0200

    Bugs fixed

--- src/teleop_vision/Detector_Modules/HandDetectorModule.py
+++ src/teleop_vision/Detector_Modules/HandDetectorModule.py
@@ -1,22 +1,24 @@
 import mediapipe as mp
-import matplotlib.pyplot as plt
-import numpy as np
 import cv2
+import numpy as np
 
-class HandDetector():
-    def __init__(self, mode=False, maxHands=2, modCompl=1, detCon=0.5, trackCon=0.5):
 
+class HandDetector:
+    def __init__(self, maxHands=2):
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(
-            static_image_mode=mode,
+            static_image_mode=False,
             max_num_hands=maxHands,
-            model_complexity=modCompl,
-            min_detection_confidence=detCon,
-            min_tracking_confidence=trackCon
+            model_complexity=1,
+            min_detection_confidence=0.7,
+            min_tracking_confidence=0.7
         )
-        self.mpDraw = mp.solutions.drawing_utils
 
-    def findHands(self, frame, draw=True, return_handedness=False):
+        self.mpDraw = mp.solutions.drawing_utils
+        self.results = None
+        self.lm_list = []
+
+    def findHands(self, frame, draw=True):
         imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
 
@@ -24,32 +26,37 @@ class HandDetector():
             for handLMs in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(
-                        frame, handLMs, self.mpHands.HAND_CONNECTIONS)
-
-        if return_handedness:
-            return frame, self.results.multi_handedness
+                        frame,
+                        handLMs,
+                        self.mpHands.HAND_CONNECTIONS
+                    )
         return frame
 
-    def findHandPosition(self, frame, hand_num=0):
-        lm_list = []
+    def findPosition(self, frame, hand_num=0):
+        self.lm_list = []
+
         if self.results.multi_hand_landmarks:
             hand = self.results.multi_hand_landmarks[hand_num]
             h, w, _ = frame.shape
 
-            for id, lm in enumerate(hand.landmark):
+            for idx, lm in enumerate(hand.landmark):
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                lm_list.append([id, cx, cy])
+                self.lm_list.append([idx, cx, cy])
 
-        return lm_list, frame
+        return self.lm_list
 
-    def findHandAperture(self, frame):
-        try:
+    def getHandCenter(self):
+        if len(self.lm_list) >= 21:
+            wrist = np.array(self.lm_list[0][1:])
+            palm = np.array(self.lm_list[9][1:])
+            return (wrist + palm) / 2
+        return None
+
+    def findAperture(self):
+        if len(self.lm_list) >= 9:
             thumb = np.array(self.lm_list[4][1:])
             index = np.array(self.lm_list[8][1:])
             dist = np.linalg.norm(index - thumb)
-
-            aperture = np.interp(dist, [20, 200], [0, 100])
-            return frame, aperture
-        except:
-            return frame, 0
+            return np.interp(dist, [20, 150], [0.0, 1.0])
+        return 0.0
     
