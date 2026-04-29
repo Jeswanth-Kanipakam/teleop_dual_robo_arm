d6e2a643841ea82d8302f8fb0729ca48f820fc26
Jeswanth_Kanipakam <jeswanthkanipakam@gmail.com>
2026-04-30 01:04:06 +0200

    Bugs fixed

--- src/teleop_vision/Detector_Modules/PoseDetectorModule.py
+++ src/teleop_vision/Detector_Modules/PoseDetectorModule.py
@@ -1,14 +1,14 @@
 import mediapipe as mp
-import numpy as np
 import cv2
+import numpy as np
 
 
-class poseDetector():
+class PoseDetector:
     def __init__(self):
-
         self.mpPose = mp.solutions.pose
-        self.pose = self.mpPose.Pose()
+        self.pose = self.mpPose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
         self.mpDraw = mp.solutions.drawing_utils
+        self.lm_list = []
 
     def findPose(self, frame, draw=True):
         imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -19,28 +19,27 @@ class poseDetector():
                 frame, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
         return frame
 
-    def findPosePosition(self, frame):
-        lm_list = []
+    def findPosition(self):
+        """Returns pose landmarks, stores in self.lm_list"""
+        self.lm_list = []
         if self.results.pose_landmarks:
-            h, w, _ = frame.shape
-
+            h, w, _ = self.mpPose = mp.solutions.pose
             for id, lm in enumerate(self.results.pose_landmarks.landmark):
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                lm_list.append([id, cx, cy])
+                self.lm_list.append([id, cx, cy])
+        return self.lm_list
 
-        self.lm_list = lm_list
-        return lm_list
+    def findAngle(self, p1, p2, p3):
+        """Find angle between three pose landmarks with safety checks"""
+        if len(self.lm_list) >= max(p1, p2, p3) + 1:
+            x1, y1 = self.lm_list[p1][1:]
+            x2, y2 = self.lm_list[p2][1:]
+            x3, y3 = self.lm_list[p3][1:]
 
-    def findAngle(self, frame, p1, p2, p3):
-        x1, y1 = self.lm_list[p1][1:]
-        x2, y2 = self.lm_list[p2][1:]
-        x3, y3 = self.lm_list[p3][1:]
+            v1 = np.array([x1 - x2, y1 - y2])
+            v2 = np.array([x3 - x2, y3 - y2])
 
-        v1 = np.array([x1 - x2, y1 - y2])
-        v2 = np.array([x3 - x2, y3 - y2])
-
-        angle = np.degrees(
-            np.arccos(np.dot(v1, v2) /
-                      (np.linalg.norm(v1) * np.linalg.norm(v2)))
-        )
-        return angle
+            angle = np.degrees(np.arccos(np.dot(v1, v2) / 
+                              (np.linalg.norm(v1) * np.linalg.norm(v2))))
+            return angle
+        return 0.0
