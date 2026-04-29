acceb3b40e58445bb25800a10bc317f23f930221
Jeswanth_Kanipakam <jeswanthkanipakam@gmail.com>
2026-04-30 01:10:57 +0200

    Final commit

--- src/teleop_vision/setup.py
+++ src/teleop_vision/setup.py
@@ -1,29 +1,48 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+from glob import glob
+import os
 
 package_name = 'teleop_vision'
 
 setup(
     name=package_name,
     version='0.0.0',
+
     packages=find_packages(exclude=['test']),
+
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.py')
+        ),
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
+
     maintainer='ud90uhak',
     maintainer_email='jeswanth.kanipakam@fau.de',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+
+    description='Dual arm vision teleoperation package',
+    license='Apache-2.0',
+
     extras_require={
-        'test': [
-            'pytest',
-        ],
+        'test': ['pytest'],
     },
+
     entry_points={
         'console_scripts': [
+            'teleop_mapper = teleop_vision.vision_node:main',
         ],
     },
 )
